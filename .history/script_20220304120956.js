abee7c0227b61e0e452015490db7d3ca99521a99
jt010051 <jont26.smith@hcl.com>
2022-03-04 12:15:10 -0500

    only commit for now

---
+++ .history/script_20220304120956.js
@@ -0,0 +1,20 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const exerpt = document.getElementById('exerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bgs_texts = document.querySelectorAll('.animated-bg-text')
+
+function getData(){
+
+    header.innerHTML =`<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt=""/>`;
+
+    title.innerHTML=`Lorem ipsum dolor sit amet.`;
+    exerpt.innerHTML=`Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, porro.`;
+    profile_img.innerHTML=`  <img src="https://randomuser.me/api/portraits/men/45.jpg" alt="">`;
+    name.innerHTML='John Doe';
+    date.innerHTML='March 04, 2022';
+}
